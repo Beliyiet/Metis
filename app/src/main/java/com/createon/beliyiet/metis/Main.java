bc7630a45059d142eed0e42bc2d2b34fb838421d
Beliyiet <beliyiet@vip.qq.com>
2017-12-19 22:58:00 +0800

    2017年12月19日 GridView为基础的首页完成 赶紧赶紧( ¯(∞)¯ )

--- app/src/main/java/com/createon/beliyiet/metis/Main.java
+++ app/src/main/java/com/createon/beliyiet/metis/Main.java
@@ -2,11 +2,6 @@ package com.createon.beliyiet.metis;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,16 +10,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 import com.createon.beliyiet.metis.adapter.CardInfo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private CoordinatorLayout mCoordinatorLayout;
+    private GridView gridView1;
+
+    private List<Map<String,Object>> dataList;
+    private int[] image = {
+            R.drawable.image_navi,
+            R.drawable.image_3d,
+            R.drawable.image_data,
+            R.drawable.image_learning,
+            R.drawable.image_programme,
+            R.drawable.image_test};
+    private String[] names = {"路线导航","3D模型","预案资料","案例学习","实战资料速查","考试系统"};
+    private SimpleAdapter simpleAdapter1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,11 +51,29 @@ public class Main extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinatorlayout);
-        List<CardInfo> mainList = new ArrayList<>();
-        mainList.add(new CardInfo(R.drawable.image_navi,"路线导航"));
+        GridView gridView1 = (GridView)findViewById(R.id.gridview1);
+        dataList = new ArrayList<Map<String, Object>>();
+        simpleAdapter1 = new SimpleAdapter(
+                this,getData(),
+                R.layout.item,new String[]{"image","names"},
+                new int[]{R.id.main_card_img1,
+                        R.id.main_card_text1});
+        gridView1.setAdapter(simpleAdapter1);
+
+    }
+
+    private List<Map<String,Object>> getData() {
+        for(int i=0;i<image.length;i++){
+            Map<String, Object> map= new HashMap<String, Object>();
+            map.put("image", image[i]);
+            map.put("names", names[i]);
+            dataList.add(map);
+        }
+        return dataList;
     }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
